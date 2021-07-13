b3ade741db49611ec56e9819d9ee9b457cf31cca
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-13 19:20:54 -0300

    Avatar component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+
+import { Avatar } from '../Avatar';
+
 import { styles } from './styles';
 
 export function Profile() {
   return (
     <View style={styles.container}>
+      
+      <Avatar urlImage="https://github.com/guilhermesandi.png" />
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -4,6 +4,7 @@ import { theme } from '../../global/styles/theme';
 export const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    alignItems: 'center',
   },
   user: {
     flexDirection: 'row',
