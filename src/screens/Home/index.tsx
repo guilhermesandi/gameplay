0d2468fe41fb6605f9e58202e03bc81c38155f73
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-13 19:58:42 -0300

    ButtonAdd component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
 
 import { styles } from './styles';
 
@@ -10,6 +11,7 @@ export function Home() {
     <View>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
       </View>
     </View>
   );
