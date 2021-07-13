a0a526f420169f8ba5743fd212ad41898d0c21d9
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-13 19:40:53 -0300

    Navigation and Routes

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   Image,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 
@@ -12,6 +13,12 @@ import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -34,6 +41,7 @@ export function SignIn() {
 
         <ButtonIcon
           title="Entrar com Discord"
+          onPress={handleSignIn}
         />
       </View>
     </View>
