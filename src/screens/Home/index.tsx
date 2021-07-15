e17536673ecb7c04e3b9c1c6a553e9edef600d16
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-14 21:03:24 -0300

    Category list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 
 import { Profile } from '../../components/Profile';
+import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
       </View>
     </View>
   );
