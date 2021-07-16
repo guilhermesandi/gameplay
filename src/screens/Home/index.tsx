6af7493d496a2513ac05c3a974b848251d137d7b
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-16 17:57:52 -0300

    Create ListHeader

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 
-import { Profile } from '../../components/Profile';
 import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+import { ListHeader } from '../../components/ListHeader';
 
 import { styles } from './styles';
 
@@ -26,6 +27,13 @@ export function Home() {
           categorySelected={category}
           setCategory={handleCategorySelect}
         />
+
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+        </View>
       </View>
     </View>
   );
