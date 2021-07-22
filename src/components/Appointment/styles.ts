ba7ea8d9b543e5ceb5edd34c80573ef5f1483fa9
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-07-21 23:45:48 -0300

    Appointment style

--- src/components/Appointment/styles.ts
+++ src/components/Appointment/styles.ts
@@ -3,5 +3,28 @@ import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  content: {
+    flex: 1,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 12,
+  },
+  title: {
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+    fontSize: 18,
+  },
+  category: {
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.highlight,
+    fontSize: 13,
+    marginRight: 24,
   },
 });
