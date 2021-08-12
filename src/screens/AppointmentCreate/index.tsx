bba205fc7915d4f90f9eebeb4bad854eeb3a75bb
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-08-12 20:40:06 -0300

    Create date and time input

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -9,8 +9,9 @@ import { styles } from './styles';
 
 import { Background } from '../../components/Background';
 import { CategorySelect } from '../../components/CategorySelect';
-import { Header } from '../../components/Header';
 import { GuildIcon } from '../../components/GuildIcon';
+import { SmallInput } from '../../components/SmallInput';
+import { Header } from '../../components/Header';
 
 
 export function AppointmentCreate() {
@@ -56,6 +57,36 @@ export function AppointmentCreate() {
             />
           </View>
         </RectButton>
+
+        <View style={styles.field}>
+          <View>
+            <Text style={styles.label}>
+              Dia e mês
+            </Text>
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>
+                /
+              </Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+
+          <View>
+            <Text style={styles.label}>
+              Hora e minuto
+            </Text>
+
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>
+                :
+              </Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+        </View>
       </View>
     </Background>
   );
