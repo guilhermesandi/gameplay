ca2ddab00042615f8e5be35a60f0cefea53129f9
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-08-12 22:01:46 -0300

    Create TextArea

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
 
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  Platform,
+  ScrollView,
+  KeyboardAvoidingView
+} from 'react-native';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
@@ -11,6 +17,7 @@ import { Background } from '../../components/Background';
 import { CategorySelect } from '../../components/CategorySelect';
 import { GuildIcon } from '../../components/GuildIcon';
 import { SmallInput } from '../../components/SmallInput';
+import { TextArea } from '../../components/TextArea';
 import { Header } from '../../components/Header';
 
 
@@ -18,76 +25,98 @@ export function AppointmentCreate() {
   const [category, setCategory] = useState('');
 
   return (
-    <Background>
-      <Header
-        title="Agendar partida"
-      />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <Background>
+        <Header
+          title="Agendar partida"
+        />
 
-      <Text style={[
-        styles.label,
-        { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}
-      >
-        Categoria
-      </Text>
+        <Text style={[
+          styles.label,
+          { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}
+        >
+          Categoria
+        </Text>
 
-      <CategorySelect
-        hasCheckBox
-        setCategory={setCategory}
-        categorySelected={category}
-      />
+        <CategorySelect
+          hasCheckBox
+          setCategory={setCategory}
+          categorySelected={category}
+        />
 
-      <View style={styles.form}>
-        <RectButton>
-          <View style={styles.select}>
-            {
-              // <View style={styles.image} />
-              <GuildIcon />
-            }
+        <View style={styles.form}>
+          <RectButton>
+            <View style={styles.select}>
+              {
+                // <View style={styles.image} />
+                <GuildIcon />
+              }
 
-            <View style={styles.selectBody}>
+              <View style={styles.selectBody}>
+                <Text style={styles.label}>
+                  Selecione um servidor
+                </Text>
+              </View>
+
+              <Feather
+                name="chevron-right"
+                color={theme.colors.heading}
+                size={18}
+              />
+            </View>
+          </RectButton>
+
+          <View style={styles.field}>
+            <View>
               <Text style={styles.label}>
-                Selecione um servidor
+                Dia e mês
               </Text>
+
+              <View style={styles.column}>
+                <SmallInput maxLength={2} />
+                <Text style={styles.divider}>
+                  /
+                </Text>
+                <SmallInput maxLength={2} />
+              </View>
             </View>
 
-            <Feather
-              name="chevron-right"
-              color={theme.colors.heading}
-              size={18}
-            />
-          </View>
-        </RectButton>
+            <View>
+              <Text style={styles.label}>
+                Hora e minuto
+              </Text>
 
-        <View style={styles.field}>
-          <View>
+              <View style={styles.column}>
+                <SmallInput maxLength={2} />
+                <Text style={styles.divider}>
+                  :
+                </Text>
+                <SmallInput maxLength={2} />
+              </View>
+            </View>
+          </View>
+
+          <View style={[styles.field, { marginBottom: 12 }]}>
             <Text style={styles.label}>
-              Dia e mês
+              Descrição
             </Text>
 
-            <View style={styles.column}>
-              <SmallInput maxLength={2} />
-              <Text style={styles.divider}>
-                /
-              </Text>
-              <SmallInput maxLength={2} />
-            </View>
-          </View>
-
-          <View>
-            <Text style={styles.label}>
-              Hora e minuto
+            <Text style={styles.caracteresLimit}>
+              Max 100 caracteres
             </Text>
-
-            <View style={styles.column}>
-              <SmallInput maxLength={2} />
-              <Text style={styles.divider}>
-                :
-              </Text>
-              <SmallInput maxLength={2} />
-            </View>
           </View>
+
+          <TextArea
+            multiline
+            maxLength={100}
+            numberOfLines={5}
+            autoCorrect={false}
+          />
         </View>
-      </View>
-    </Background>
+      </Background>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -51,5 +51,10 @@ export const styles = StyleSheet.create({
     fontSize: 15,
     fontFamily: theme.fonts.text500,
     color: theme.colors.highlight,
+  },
+  caracteresLimit: {
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    color: theme.colors.highlight,
   }
 })
