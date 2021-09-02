93a1b3c9d1e75db0086ec16a99b58ee66564e7e7
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-09-01 22:22:02 -0300

    Show server image

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { View, Image } from 'react-native';
 
 import { styles } from './styles';
+import DiscordSvg from '../../assets/discord.svg';
 
-export function GuildIcon() {
-  const uri = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCNWZAJTiIoIZX_8NTc6_FgFLmQF-gNxLK2A&usqp=CAU';
+const { CDN_IMAGE } = process.env;
+
+type Props = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
 
   return (
-    <Image
-      source={{ uri }}
-      style={styles.image}
-      resizeMode="cover"
-    />
+    <View style={styles.container}>
+      {
+        iconId ?
+          <Image
+            source={{ uri }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          :
+          <DiscordSvg
+            width={40}
+            height={40}
+          />
+      }
+    </View>
   )
 }
