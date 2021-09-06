4cd0c31399d0eb9a2b034c95979d82a5b605700c
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-09-06 20:58:46 -0300

    Submit form

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import uuid from 'react-native-uuid';
 
 import {
   Text,
@@ -10,6 +13,7 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
@@ -25,11 +29,18 @@ import { Guilds } from '../Guilds';
 
 import { GuildProps } from '../../components/Guild';
 
-
 export function AppointmentCreate() {
   const [category, setCategory] = useState('');
   const [openGuildsModal, setOpenGuildsModal] = useState(false);
   const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigation = useNavigation();
 
   function handleOpenGuilds() {
     setOpenGuildsModal(true);
@@ -46,6 +57,26 @@ export function AppointmentCreate() {
 
   function handleCategorySelect(categoryId: string) {
     setCategory(categoryId);
+  }
+
+  async function handleSave() {
+    const newAppointment = {
+      id: uuid.v4(),
+      guild,
+      category,
+      date: `${day}/${month} às ${hour}:${minute}h`,
+      description
+    };
+
+    const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const appointments = storage ? JSON.parse(storage) : [];
+
+    await AsyncStorage.setItem(
+      COLLECTION_APPOINTMENTS,
+      JSON.stringify([...appointments, newAppointment])
+    );
+
+    navigation.navigate('Home');
   }
 
   return (
@@ -77,7 +108,7 @@ export function AppointmentCreate() {
               <View style={styles.select}>
                 {
                   guild.icon
-                    ? <GuildIcon />
+                    ? <GuildIcon guildId={guild.id} iconId={guild.icon} />
                     : <View style={styles.image} />
                 }
 
@@ -106,11 +137,17 @@ export function AppointmentCreate() {
                 </Text>
 
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setDay}
+                  />
                   <Text style={styles.divider}>
                     /
                   </Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setMonth}
+                  />
                 </View>
               </View>
 
@@ -120,11 +157,17 @@ export function AppointmentCreate() {
                 </Text>
 
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setHour}
+                  />
                   <Text style={styles.divider}>
                     :
                   </Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput
+                    maxLength={2}
+                    onChangeText={setMinute}
+                  />
                 </View>
               </View>
             </View>
@@ -144,11 +187,15 @@ export function AppointmentCreate() {
               maxLength={100}
               numberOfLines={5}
               autoCorrect={false}
+              onChangeText={setDescription}
             />
           </View>
 
           <View style={styles.footer}>
-            <Button title="Agendar" />
+            <Button
+              title="Agendar"
+              onPress={handleSave}
+            />
           </View>
         </ScrollView>
       </Background>
