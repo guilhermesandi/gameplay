0608806234cc18cd7986bdedaa8d0bc9cb6a9d2d
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-09-14 20:07:23 -0300

    listing appointments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,48 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
+import { Load } from '../../components/Load';
 import { ListHeader } from '../../components/ListHeader';
 import { Background } from '../../components/Background';
-import { Appointment } from '../../components/Appointment';
+import { Appointment, AppointmentProps } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
 
 import { styles } from './styles';
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 
 export function Home() {
   const [category, setCategory] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
 
   const navigation = useNavigation();
-
-  const appointments = [
-    {
-      id: '1',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    },
-    {
-      id: '2',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-    }
-  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -56,6 +35,23 @@ export function Home() {
     navigation.navigate('AppointmentCreate');
   }
 
+  async function loadAppointments() {
+    const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const storage: AppointmentProps[] = response ? JSON.parse(response) : [];
+
+    if (category) {
+      setAppointments(storage.filter(item => item.category === category));
+    } else {
+      setAppointments(storage);
+    }
+
+    setLoading(false);
+  }
+
+  useFocusEffect(useCallback(() => {
+    loadAppointments();
+  }, [category]));
+
   return (
     <Background>
       <View style={styles.header}>
@@ -68,25 +64,30 @@ export function Home() {
         setCategory={handleCategorySelect}
       />
 
-      <ListHeader
-        title="Partidas agendadas"
-        subtitle="Total 6"
-      />
+      {
+        loading ? <Load /> :
+          <>
+            <ListHeader
+              title="Partidas agendadas"
+              subtitle="Total 6"
+            />
 
-      <FlatList
-        data={appointments}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <Appointment
-            data={item}
-            onPress={handleAppointmentDetails}
-          />
-        )}
-        ItemSeparatorComponent={() => <ListDivider />}
-        contentContainerStyle={{ paddingBottom: 69 }}
-        style={styles.matches}
-        showsVerticalScrollIndicator={false}
-      />
+            <FlatList
+              data={appointments}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => (
+                <Appointment
+                  data={item}
+                  onPress={handleAppointmentDetails}
+                />
+              )}
+              ItemSeparatorComponent={() => <ListDivider />}
+              contentContainerStyle={{ paddingBottom: 69 }}
+              style={styles.matches}
+              showsVerticalScrollIndicator={false}
+            />
+          </>
+      }
     </Background>
   );
 }
