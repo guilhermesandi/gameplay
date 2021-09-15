e4cb3c669ca8bd11dc5071a98bd78e72a3508044
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-09-15 20:24:27 -0300

    SignOut function

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
 import { useAuth } from '../../hooks/auth';
 
 import { Avatar } from '../Avatar';
@@ -7,12 +9,28 @@ import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  function handleSignOut() {
+    Alert.alert('Logout', 'Deseja sair do GamePlay?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel'
+        },
+        {
+          text: 'Sim',
+          onPress: () => signOut()
+        }
+      ])
+  }
 
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleSignOut}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
 
       <View>
         <View style={styles.user}>
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -32,6 +32,7 @@ type AuthContextData = {
   user: User;
   loading: boolean;
   signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -85,6 +86,11 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signOut() {
+    setUser({} as User);
+    await AsyncStorage.removeItem(COLLECTION_USERS);
+  }
+
   async function loadUserStorageData() {
     const storage = await AsyncStorage.getItem(COLLECTION_USERS);
 
@@ -105,6 +111,7 @@ function AuthProvider({ children }: AuthProviderProps) {
       user,
       loading,
       signIn,
+      signOut,
     }}>
       {children}
     </AuthContext.Provider>
